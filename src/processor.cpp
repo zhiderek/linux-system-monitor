0c3ff8bdd994f2eca1c6c28811b807929d1dfa2b
derek zhi <zhiderek@gmail.com>
2020-04-20 22:33:37 -0700

    sync

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,12 @@
 #include "processor.h"
-
+#include "linux_parser.h"
+#include <iostream>
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+	int prevActive = curtActive;
+	int prevTotal = curtTotal;
+	curtActive = LinuxParser::ActiveJiffies();
+	curtTotal = LinuxParser::Jiffies();
+	float res = (curtActive - prevActive) / (curtTotal - prevTotal + epsilon);
+	return res;	
+}
