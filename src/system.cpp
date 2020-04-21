355c4ee06fd431e42baf439fd76145f8f1e28b0e
derek zhi <zhiderek@gmail.com>
2020-04-20 22:33:58 -0700

    sync

--- src/system.cpp
+++ src/system.cpp
@@ -17,7 +17,13 @@ using std::vector;
 Processor& System::Cpu() { return cpu_; }
 
 // Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() {
+	vector<int> pids = LinuxParser::Pids();
+	for (int pid : pids) {
+		_processes.emplace_back(new Process(pid));
+	}
+	return _process;	
+ }
 
 // DONE: Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
