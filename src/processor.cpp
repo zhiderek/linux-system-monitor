50953986152086b91071d15233422b4639a4d1fa
derek zhi <zhiderek@gmail.com>
2020-04-22 18:08:34 -0700

    simpler implementation of cpu utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() {
-	int prevActive = curtActive;
-	int prevTotal = curtTotal;
+	//int prevActive = curtActive;
+	//int prevTotal = curtTotal;
 	curtActive = LinuxParser::ActiveJiffies();
 	curtTotal = LinuxParser::Jiffies();
-	float res = (curtActive - prevActive) / (curtTotal - prevTotal + epsilon);
+	float res = 1.0 * curtActive / curtTotal;
+	//float res = 1.0 * (curtActive - prevActive) / (curtTotal - prevTotal + epsilon);
 	return res;	
 }
